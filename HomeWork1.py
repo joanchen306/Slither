cd70eb01d63d97b7560d0b989409e54bf4e35113
Joan Chen <joan.chen@gatech.edu>
2019-12-22 22:26:06 +0800

    updated hw1

--- HomeWork1.py
+++ HomeWork1.py
@@ -1,18 +1,18 @@
-#問題1
-#getScore(score)という関数を作って点数を SCORE: 20 みたいに表示しましょう
-#ヒント：戻り値のタイプを考えてみましょう
+
+
+# 問題1
+# getScore(score)という関数を作って点数を SCORE: 20 みたいに表示しましょう
+# ヒント：戻り値のタイプを考えてみましょう
 def getScore(score):
 	return("SCORE: " + str(score))
 
-
-#問題2
-#addScoreを書き直してりんごを食べたら点数を稼げるようにしましょう
+# 問題2
+# addScoreを書き直してりんごを食べたら点数を稼げるようにしましょう
 def addScore(score):
 	score = score + 20
 	return score
-	
 
-#問題3
-#getSpeed()という関数を作ってへびのスピードを変わりましょう
+# 問題3
+# getSpeed()という関数を作ってへびのスピードを変わりましょう
 def getSpeed():
 	return 15
